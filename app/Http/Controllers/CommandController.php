3fd841cfa1ff94009f74e5db83650fa10879c7ff
Coderman007 <coderman1980@gmail.com>
2024-07-01 13:00:39 -0500

    Assets Production

---
+++ app/Http/Controllers/CommandController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Request;
+
+class CommandController extends Controller
+{
+    public function linkStorage()
+    {
+        // Ejecuta el comando storage:link
+        $exitCode = Artisan::call('storage:link');
+
+        // Puedes verificar si el comando fue exitoso
+        if ($exitCode === 0) {
+            return response()->json(['success' => true, 'message' => 'El enlace simbólico se creó correctamente.']);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Hubo un problema al crear el enlace simbólico.']);
+        }
+    }
+
+    public function clearCache(Request $request)
+    {
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
+        Artisan::call('view:clear');
+
+        return response()->json(['success' => true, 'message' => 'Toda la caché ha sido borrada.']);
+    }
+
+}
