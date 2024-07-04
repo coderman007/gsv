c01440d2a6daf39b71f8db8ee303556fbe003def
Coderman007 <coderman1980@gmail.com>
2024-07-03 21:55:04 -0500

    Logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,24 +1,22 @@
-<div class="">
-
-    <div class="rounded flex items-center">
+<div class="rounded flex items-center" style="color: purple;">
         <a href="{{ auth()->check() ? route('dashboard') : route('welcome') }}" class="flex rounded-lg bg-transparent items-center">
         <img src="{{ asset('images/logo.jpg') }}" width="50"
                  class="transition duration-700 ease-in-out rounded-full shadow-white-2xl brightness-90 hover:brightness-150"
                  alt="Logo GSV">
+            <div class="slogan text-center ml-1">
+                <div class="">
+                    <h1 class="my-[-3px] text-[13px]">
+                        <span class="text-sm font-extrabold">G</span>estión energética y
+                    </h1>
+                    <h1 class="my-[-3px] text-[13px]">
+                        <span class="text-sm font-extrabold">S</span>oluciones <span class="text-sm font-extrabold">V</span>erdes
+                    </h1>
+                </div>
+                <div class="">
+                    <h1 class="my-[-3px] text-[9px]">con ingeniería</h1>
+                </div>
+            </div>
         </a>
-        <div class="slogan text-center ml-1">
-            <div class="">
-                <h1 class="my-[-3px] text-[13px]">
-                    <span class="text-sm font-extrabold">G</span>estión energética y
-                </h1>
-                <h1 class="my-[-3px] text-[13px]">
-                    <span class="text-sm font-extrabold">S</span>oluciones <span class="text-sm font-extrabold">V</span>erdes
-                </h1>
-            </div>
-            <div class="">
-                <h1 class="my-[-3px] text-[9px]">con ingeniería</h1>
-            </div>
-        </div>
+
     </div>
 
-</div>
